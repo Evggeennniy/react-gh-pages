295b47c31d8815c249fa147444a84273ccac92d1
FiX <myiphone128@mail.ru>
2024-05-09 22:50:40 +0300

    homework34 new exec

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,80 +1,48 @@
 import "./Main.css";
 import { Img } from "../Img";
 import { useState } from "react";
+import { arrTabsData } from "../../data";
 
 export const Main = function () {
   let [clickersData, setCount] = useState({});
+  let [tabsData, setTabsData] = useState(arrTabsData);
 
-  const updateCounter = (key) => {
-    let countOfClicks = clickersData[key] || 0;
-    countOfClicks++;
-    clickersData[key] = countOfClicks;
-    setCount(clickersData);
+  const updateData = (key) => {
+    const newClickersData = { ...clickersData };
+    let countOfClicks = (clickersData[key] || 0) + 1;
+    newClickersData[key] = countOfClicks;
+    setCount(newClickersData);
+  };
 
-    return countOfClicks;
+  const handleShowResults = () => {
+    const maxClicks = Math.max(...Object.values(clickersData));
+    const filteredData = tabsData.filter(
+      (item) => clickersData[item.key] === maxClicks
+    );
+
+    if (filteredData.length > 1) {
+      alert("Multiple items have the maximum click count.");
+    }
+
+    setTabsData(filteredData);
   };
 
   return (
     <main>
       <h2>Main Container</h2>
-      <div className="img__wrapper sad_smile">
-        <Img
-          src={
-            "https://static.vecteezy.com/system/resources/previews/022/461/791/original/disappointed-emoji-sad-face-unhappy-emoticon-vector.jpg"
-          }
-          onClick={(e) => {
-            const countOfClicks = updateCounter("sad_smile");
-            const outputEl = e.target.parentElement.querySelector(
-              "p.img__wrapper-output"
-            );
-            outputEl.textContent = countOfClicks;
-          }}
-        />
-        <p className="img__wrapper-output">0</p>
-      </div>
-      <div className="img__wrapper neutral_smile">
-        <Img
-          src={
-            "https://listemoji.com/img/emoji/using/neutral-face-1f610-facebook.png"
-          }
-          onClick={(e) => {
-            const countOfClicks = updateCounter("neutral_smile");
-            const outputEl = e.target.parentElement.querySelector(
-              "p.img__wrapper-output"
-            );
-            outputEl.textContent = countOfClicks;
-          }}
-        />
-        <p className="img__wrapper-output">0</p>
-      </div>
-      <div className="img__wrapper happy_smile">
-        <Img
-          src={
-            "https://emojiisland.com/cdn/shop/products/Emoji_Icon_-_Happy_large.png?v=1571606093"
-          }
-          onClick={(e) => {
-            const countOfClicks = updateCounter("happy_smile");
-            const outputEl = e.target.parentElement.querySelector(
-              "p.img__wrapper-output"
-            );
-            outputEl.textContent = countOfClicks;
-          }}
-        />
-        <p className="img__wrapper-output">0</p>
-      </div>
-      <button
-        className="show-results"
-        onClick={() => {
-          const theMostClickableImg = Object.keys(clickersData).reduce((a, b) =>
-            clickersData[a] > clickersData[b] ? a : b
-          );
-          const imgWrappers = document.querySelectorAll(".img__wrapper");
-          imgWrappers.forEach((item) => {
-            console.log(item);
-            if (!item.classList.contains(theMostClickableImg)) item.remove();
-          });
-        }}
-      >
+      {tabsData.map((item) => (
+        <div key={item.key} className="img__wrapper">
+          <Img
+            src={item.src}
+            alt=""
+            onClick={() => {
+              updateData(item.key);
+            }}
+          />
+          <p className="img__wrapper-output">{clickersData[item.key] || 0}</p>
+        </div>
+      ))}
+      <button className="show-results" onClick={handleShowResults}>
         Show Results
       </button>
     </main>
